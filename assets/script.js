ccbd53191f17eb5aa886f5cfc1a419f0186f3968
ChrisJCota <chrisjocota@gmail.com>
2023-02-15 18:54:58 -0500

    initial JS w/variables

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+const myKey = 'b1cd3692';
+
+var movieInput = document.getElementById("searchInput");
+
+var yearX = document.getElementById("yearXBox");
+var castX = document.getElementById("castXBox");
+var infoX = document.getElementById("infoXBox");
+var ratingX = document.getElementById("ratingXBox");
+
+var moviesSearched = document.getElementById("movieList");
+
+var apiOMDB = "http://www.omdbapi.com/?i=tt3896198&apikey=" + myKey;
