773b6449143ae548890132828e324123d22317d0
abc <abc@example.com>
2023-02-15 20:52:32 -0500

    'owens first commit'

--- assets/script.js
+++ assets/script.js
@@ -13,6 +13,11 @@ var moviesSearched = document.getElementById("movieList");
 
 //t=title, i=imdb id, type=movies,series,episode y=year 
 var apiOMDB = "http://www.omdbapi.com/?t="+movieInput.value+"&apikey=b1cd3692";
+var apiYouTube = "https://www.googleapis.com/youtube/v3/search?part=snippet&q="+ movieInput.val() + "&apiKey=AIzaSyDhMGoaM3JNvpeBffOSpxTvwHWi2PvlTDM";
 
-
+fetch(apiYouTube, { method: "GET" })
+.then((response) => response.json())
+.then((data) => {
+  console.log(data);
+});
 
