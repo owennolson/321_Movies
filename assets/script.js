f46b03cc640b6afb0e1eb1d98f7833a9f454ae8c
ChrisJCota <chrisjocota@gmail.com>
2023-02-15 19:28:03 -0500

    Button for searching

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 const myKey = 'b1cd3692';
 
 var movieInput = document.getElementById("searchInput");
+var searchBTN = document.getElementById("SearchBtn");
 
 var yearX = document.getElementById("yearXBox");
 var castX = document.getElementById("castXBox");
@@ -9,6 +10,8 @@ var ratingX = document.getElementById("ratingXBox");
 
 var moviesSearched = document.getElementById("movieList");
 
+
+//t=title, i=imdb id, type=movies,series,episode y=year 
 var apiOMDB = "http://www.omdbapi.com/?t="+movieInput.value+"&apikey="+myKey;
 
 
