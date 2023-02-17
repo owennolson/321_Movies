b5d1231424a6679e58a4d41a98010e071f236be8
ChrisJCota <chrisjocota@gmail.com>
2023-02-16 21:14:00 -0500

    updated JS for Main page

--- assets/script.js
+++ assets/script.js
@@ -9,7 +9,7 @@ var infoX = document.getElementById("infoXBox");
 var ratingX = document.getElementById("ratingXBox");
 
 var moviesSearched = document.getElementById("movieList");
-
+console.log(moviesSearched);
 
 //t=title, i=imdb id, type=movies,series,episode y=year plot=return short or full
 
@@ -20,13 +20,60 @@ function getMovieInfo(){
     fetch(apiOMDB)
     .then(function (response) {
       if (response.ok) {
-        console.log(response);
+        
         response.json().then(function (data) {
           console.log(data);
+          
+          var posterList = document.createElement("img");
+          var getPoster = data.Poster;
+
+          posterList.src = getPoster;
+          moviesSearched.appendChild(posterList);
+
+
+          if (yearX.checked) {
+            // this element is checked
+            var yearList = document.createElement("li");
+            var getYr = "Cast: " + Number(data.Year);
+            yearList.textContent = getYr;
+            
+            moviesSearched.appendChild(yearList);
+        }
+          if(castX.checked){
+            var castList = document.createElement("li");
+            var getCast = "Cast: " + data.Actors;
+            castList.textContent = getCast;
+
+            moviesSearched.appendChild(castList);
+          }
+          if(infoX.checked){
+            var infoList = document.createElement("li");
+            var getInfo = "Info: " + data.Plot;
+            infoList.textContent = getInfo;
+
+            moviesSearched.appendChild(infoList);
+          }
+          if(ratingX.checked){
+            var ratingList = document.createElement("li");
+            var getRatings = "imdbRatings: " + data.imdbRating;
+            ratingList.textContent = getRatings;
+            moviesSearched.appendChild(ratingList);
+          }
+
           
         });
       } 
 }
 )};
+
+//t=title, i=imdb id, type=movies,series,episode y=year 
+var apiOMDB = "http://www.omdbapi.com/?t="+movieInput.value+"&apikey=b1cd3692";
+var apiYouTube = "https://www.googleapis.com/youtube/v3/search?part=snippet&q="+ movieInput.val() + "&apiKey=AIzaSyDhMGoaM3JNvpeBffOSpxTvwHWi2PvlTDM";
+
+fetch(apiYouTube, { method: "GET" })
+.then((response) => response.json())
+.then((data) => {
+  console.log(data);
+});
 
 
