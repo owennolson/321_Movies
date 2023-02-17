a4fbf0399d988cdebbb75e6dd87243b99cc50d1a
abc <abc@example.com>
2023-02-16 21:05:15 -0500

    got the youtube api working

--- assets/script.js
+++ assets/script.js
@@ -10,35 +10,8 @@ var ratingX = document.getElementById("ratingXBox");
 
 var moviesSearched = document.getElementById("movieList");
 
-
-
-//t=title, i=imdb id, type=movies,series,episode y=year plot=return short or full
-
-
-function getMovieInfo(){
-    var apiOMDB = "http://www.omdbapi.com/?t="+movieInput.value+"&apikey=b1cd3692";
-
-    fetch(apiOMDB)
-    .then(function (response) {
-      if (response.ok) {
-        console.log(response);
-        response.json().then(function (data) {
-          console.log(data);
-          
-        });
-      } 
-}
-)};
-
-
+movieInput = "avatar 2";
 //t=title, i=imdb id, type=movies,series,episode y=year 
 var apiOMDB = "http://www.omdbapi.com/?t="+movieInput.value+"&apikey=b1cd3692";
-var apiYouTube = "https://www.googleapis.com/youtube/v3/search?part=snippet&q="+ movieInput.val() + "&apiKey=AIzaSyDhMGoaM3JNvpeBffOSpxTvwHWi2PvlTDM";
-
-fetch(apiYouTube, { method: "GET" })
-.then((response) => response.json())
-.then((data) => {
-  console.log(data);
-});
 
 
